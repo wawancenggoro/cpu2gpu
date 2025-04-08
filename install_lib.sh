94493fb8c825e7b15797efcc4c067be202b52ec2
Wawan Cenggoro <wawan.cenggoro@lintasarta.co.id>
2025-04-08 16:46:00 +0700

    fix install_lib.sh

--- install_lib.sh
+++ install_lib.sh
@@ -1,5 +1,5 @@
-pip install -y xarray[complete]=='2023.11.0'
+pip install xarray[complete]=='2023.11.0'
 conda install -c conda-forge pynio
-pip install -y pandas
-pip install -y cupy
-pip install -y torch
+pip install pandas
+pip install cupy
+pip install torch
